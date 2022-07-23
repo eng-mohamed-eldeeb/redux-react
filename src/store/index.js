e55425c1f62f808edb2d7def3877f07c3e2c6f00
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-07-23 19:00:27 +0200

    final touch

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,30 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === "INC") {
     return {
-        counter: state.counter ++
-    }
+      counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
   }
-  if(action.type === 'DEC') {
+  if (action.type === "INC5") {
     return {
-        counter: state.counter--
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
+  if (action.type === "DEC") {
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+  if (action.type === 'TOGGLE') {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter
     }
   }
   return state;
